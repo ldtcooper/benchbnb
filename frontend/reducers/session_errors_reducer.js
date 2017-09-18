ff053a6626752c82453f9e8ad8a6b5ec5208dbae
Logan Cooper <ldtcoop@gmail.com>
2017-09-18 15:28:46 -0700

    Add errors and root reducers

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,1 +1,18 @@
-import { RECEIVE_ERRORS } from '../actions/session_actions';
+import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS } from '../actions/session_actions';
+
+const SessionErrorsReducer = (state = {}, action) => {
+  Object.freeze(state);
+  let newState = Object.assign({}, state);
+  switch (action.type) {
+    case RECEIVE_ERRORS:
+      newState.errors = action.errors;
+      return newState;
+    case RECEIVE_CURRENT_USER:
+      newState.errors = [];
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default SessionErrorsReducer;
