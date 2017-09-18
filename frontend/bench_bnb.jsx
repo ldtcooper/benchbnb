7dbd8096744d77b0f2d71e4011ddad2edc0ff0ed
Logan Cooper <ldtcoop@gmail.com>
2017-09-18 15:40:52 -0700

    Write store

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as APIUtil from './util/session_api_util.js';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
   window.login = APIUtil.login;
   window.signup = APIUtil.signup;
   window.logout = APIUtil.logout;
+  window.store = store;
 });
