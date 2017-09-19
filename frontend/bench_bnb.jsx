d2093f1ea0408925ec5f5f2e975938f305aca65a
Logan Cooper <ldtcoop@gmail.com>
2017-09-18 17:25:05 -0700

    Create Root component

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import { login, signup, logout } from './actions/session_actions';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const store = configureStore();
-  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
-  window.login = login;
-  window.signup = signup;
-  window.logout = logout;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  ReactDOM.render(<Root store = { store }/>, root);
 });
